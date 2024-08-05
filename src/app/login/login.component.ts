caebfa402bd149cd717d1234db73357af07d9ca5
Nico Jones <nico@speal.ai>
2024-08-05 22:41:28 +0200

    fix colors and images

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,11 @@
 import { FB_UI_CONFIG, fbUi } from "@/firebase";
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   standalone: true,
+  encapsulation: ViewEncapsulation.None,
 })
 export class LoginComponent implements AfterViewInit {
 
