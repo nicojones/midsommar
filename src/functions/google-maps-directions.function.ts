ed6b3128f8cfbf13a115ddd8e463360989dd0c20
Nico Jones <nico@speal.ai>
2024-12-29 18:59:53 +0100

    Fixed description bugs

--- src/functions/google-maps-directions.function.ts
+++ src/functions/google-maps-directions.function.ts
@@ -1,13 +1,13 @@
 import { ICoords } from "@/types";
 import { toQuery } from "./to-query.function";
 
-export const googleMapsDirections = (origin: ICoords, destination: ICoords, travelMode: "TRANSIT" | "DRIVING"): string =>
+export const googleMapsDirections = (origin: ICoords, destination: ICoords, travelMode: "TRANSIT" | "DRIVING", departureTime?: number): string =>
   "https://www.google.com/maps/dir/" + toQuery({
     api: 1,
     origin: Object.values(origin).join(","),
     destination: Object.values(destination).join(","),
     travelmode: travelMode,
-    // departure_time: departureTime ? Math.round(+departureTime / 1000) : undefined,
+    departure_time: departureTime ? Math.round(+departureTime / 1000) : undefined,
     // ttype: departureTime ? "arr" : undefined,
     // time: "15:00",
     // date: departureTime ? toIsoDate(departureTime) : undefined,
